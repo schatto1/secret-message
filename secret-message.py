5b4eacce7db0aef60a2685baaa90e7663c535fee
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-16 22:44:54 -0400

    Adds print statements to follow file renaming
    Makes OS return to initial working directory after operation is over.

--- secret-message.py
+++ secret-message.py
@@ -5,11 +5,15 @@ def rename_files():
     file_location = r"/home/dzshu49/python-foundations/secret-message/prank/prank"
     file_list = os.listdir(file_location)
     print(file_list)
+    initial_path = os.getcwd()
     os.chdir(file_location)
     
     # 2) for each file, rename
     for file_name in file_list:
         new_name = file_name.translate(None, "0123456789")
+        print("Old Name: "+file_name)
+        print("New Name: "+new_name)
         os.rename(file_name, new_name)
+    os.chdir(initial_path)
 
 rename_files()
